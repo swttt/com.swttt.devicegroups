0419d7e85c340c7d22a668b983e2ca638cc88989
Bas Jansen <b@sjansen.email>
2017-08-20 01:40:11 +0200

    1.0.0

--- drivers/devicegroup/device.js
+++ drivers/devicegroup/device.js
@@ -1,9 +1,54 @@
 'use strict';
 
 const Homey = require('homey');
-
+const {
+  HomeyAPI
+} = require('../../lib/athom-api.js');
 
 class DeviceGroupDevice extends Homey.Device {
+  onInit() {
+    this.log('device init');
+
+    this.registerMultipleCapabilityListener(this.getCapabilities(), async(valueObj, optsObj) => {
+      try {
+        var api = await this.getApi();
+        await api.devices.subscribe();
+        var deviceGroup = await this.getSettings().groupedDevices;
+        for (var key in deviceGroup) {
+          let device = await api.devices.getDevice({
+            id: deviceGroup[key].id
+          });
+          for (var key in valueObj) {
+            device.setCapabilityValue(key, valueObj[key]);
+          }
+
+        }
+        return Promise.resolve();
+      }
+      catch (err) {
+        this.error(err);
+        return Promise.reject();
+      }
+    }, 500);
+
+  }
+  // this method is called when the Device is added
+  onAdded() {
+    this.log('device added');
+  }
+
+  // this method is called when the Device is deleted
+  onDeleted() {
+    this.log('device deleted');
+  }
+
+  getApi() {
+    if (!this.api) {
+      this.api = HomeyAPI.forCurrentHomey();
+    }
+    return this.api;
+  }
+
 
 }
 
--- drivers/devicegroup/driver.js
+++ drivers/devicegroup/driver.js
@@ -2,12 +2,20 @@
 
 const Homey = require('homey');
 
+function guid() {
+ 		function s4() {
+ 			return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+ 		}
+ 		return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+ 	}
+
 class DeviceGroupDriver extends Homey.Driver {
 
   onPair( socket ) {
       let pairingDevice = {};
           pairingDevice.name = 'Grouped device';
           pairingDevice.settings = {};
+          pairingDevice.data = {};
           socket.on('addClass', function( data, callback ) {
               pairingDevice.class = data.class;
               callback( null, pairingDevice );
@@ -30,6 +38,10 @@ class DeviceGroupDriver extends Homey.Driver {
           });
           socket.on('devicesChanged', function( data, callback ) {
               pairingDevice.settings.groupedDevices = data.devices;
+              callback( null, pairingDevice );
+          });
+          socket.on('allmostDone', function( data, callback ) {
+              pairingDevice.data.id = guid();
               callback( null, pairingDevice );
           });
 
