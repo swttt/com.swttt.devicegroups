88a169150fd90d7f99562899c54323c4349a3ef4
Jamie Peake <jamie@hive.com.au>
2018-10-10 23:21:06 +1000

    code style

--- api.js
+++ api.js
@@ -4,71 +4,71 @@ const HomeyLite = require('./lib/homey-lite/lib');
 module.exports = [
   {
     // Get all groups.
-    method: 'GET',
-    path  : '/group',
-    fn    : async (args) => {
+    method : 'GET',
+    path   : '/group',
+    fn     : async (args) => {
       let groups = await Homey.app.getGroups();
       return groups.map(group => ({
-        id   : group.getData().id,
-        name : group.getName(),
-        class: group.getClass(),
+        id    : group.getData().id,
+        name  : group.getName(),
+        class : group.getClass(),
       }));
     }
   },
   {
     // Get a specific group.
-    method: 'GET',
-    path  : '/group/:id',
-    fn    : async (args) => {
+    method : 'GET',
+    path   : '/group/:id',
+    fn     : async (args) => {
       let group = await Homey.app.getGroup(args.params.id);
       return {
-        id          : args.params.id,
-        name        : group.getName(),
-        class       : group.getClass(),
-        capabilities: group.getCapabilities(),
-        data        : group.getData(),
-        settings    : group.getSettings(),
+        id           : args.params.id,
+        name         : group.getName(),
+        class        : group.getClass(),
+        capabilities : group.getCapabilities(),
+        data         : group.getData(),
+        settings     : group.getSettings(),
       };
     }
   },
   {
     // Update the grouped devices for a group.
-    method: 'PUT',
-    path  : '/group/:id',
-    fn    : async (args) => {
+    method : 'PUT',
+    path   : '/group/:id',
+    fn     : async (args) => {
 
       await Homey.app.setDevicesForGroup(args.params.id, args.body);
     }
   },
   {
     // Update the grouped devices for a group.
-    method: 'PUT',
-    path  : '/group/:id/settings',
-    fn    : async (args) => {
+    method : 'PUT',
+    path   : '/group/:id/settings',
+    fn     : async (args) => {
       await Homey.app.setGroupSettings(args.params.id, args.body);
     }
   },
   {
     // Update the grouped devices for a group.
-    method: 'PUT',
-    path  : '/group/:id/capabilities',
-    fn    : async (args) => {
+    method : 'PUT',
+    path   : '/group/:id/capabilities',
+    fn     : async (args) => {
       await Homey.app.setMethodForCapabilityOfGroup(args.params.id, args.body);
     }
   },
   {
     // Get a list of all devices.
-    method: 'GET',
-    path  : '/devices',
-    fn    : async (args) => {
+    method : 'GET',
+    path   : '/devices',
+    fn     : async (args) => {
       let devices = await Homey.app.getDevices();
       return Object.values(devices);
     }
   },
   {
-    method: 'GET',
-    path  : '/library',
-    fn    : async (args) => {
+    method : 'GET',
+    path   : '/library',
+    fn     : async (args) => {
       let library = new HomeyLite();
       return await library.getJSON();
     }
